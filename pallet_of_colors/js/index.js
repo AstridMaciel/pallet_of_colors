23e28619add69c58f69fbf58622bb7cc22331e1e
Astrid Maciel <astridmaciel12@gmail.com>
2017-09-16 11:13:30 -0500

    pallet of colors

---
+++ pallet_of_colors/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".c-red").click(function(){
+        $(".b-red").toggle();
+    });
+    $(".c-blue").click(function(){
+        $(".b-blue").toggle();
+    });
+    $(".c-yellow").click(function(){
+        $(".b-yellow").toggle();
+    });
+});
